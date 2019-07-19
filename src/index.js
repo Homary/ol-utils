8f2237dba7ad17a12597910465578cf9be56e06e
Homary <935124541@qq.com>
2019-07-19 09:11:55 +0800

    feat: 修改 addCustomEvent
    1. addCustomEvent 无需传递 Feature, FeatureEvent 参数;
    2. 修改文档说明

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import { render } from 'ol-utils';
-import { createCluster, createLayer, createFeature, createOverlay } from '@/components';
-import { Style } from 'ol/style';
+import {
+	render,
+	createLayer,
+	createFeature,
+	addEvent
+} from 'ol-utils';
+import { addCustomEvent } from '@/components';
+
+import {Style, Circle, Fill} from 'ol/style';
 
 import orange from '@/img/orange.png';
 
@@ -9,35 +15,35 @@ window.onload = function() {
 		'http://www.google.cn/maps/vt/pb=!1m4!1m3!1i{z}!2i{x}!3i{y}!2m3!1e0!2sm!3i380072576!3m8!2szh-CN!3scn!5e1105!12m4!1e68!2m2!1sset!2sRoadmap!4e0!5m1!1e0';
 	let elID = 'app';
 	let center = [112.55, 28.27];
-
 	let map = render(elID, url, center);
-
-	let features = [];
-
-	for (let i = 0; i < 50; i++) {
-		let lon = 112.55 + i * 0.001;
-		let lat = 28.27 + i * Math.random() * 0.005;
-		let feature = createFeature([lon, lat]);
-		features.push(feature);
-	}
-
-	let cluster = createCluster(features, orange, 'test');
-    map.addLayer(cluster);
+    let feature = createFeature(center, orange);
     
-    addDialog();
+    // 设置值
+    feature.set('greet', 'this is a icon on map');
 
-    let overlay = createOverlay('over', center, [0, 0]);
+	let layer = createLayer();
+    
+	layer.getSource().addFeature(feature);
+	map.addLayer(layer);
 
-    map.addOverlay(overlay)
+	// 添加事件
+	addEvent(map, 'singleclick', (e) => {
+        console.log(e);
+        
+        // 获得点位的值
+        let greet = e.selected[0].get('greet');
+
+	    alert(greet);
+    })
 };
 
 function addDialog() {
-   let div = document.createElement('div');
+	let div = document.createElement('div');
 
-   div.id = 'over';
-   div.style.width = '300px';
-   div.style.height = '300px';
-   div.style.background = '#666';
+	div.id = 'over';
+	div.style.width = '300px';
+	div.style.height = '300px';
+	div.style.background = '#666';
 
-   document.getElementById('app').appendChild(div);
+	document.getElementById('app').appendChild(div);
 }
